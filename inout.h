62cebdf9c913244bce464a77c5fd843a45f18916
fLUM1NUS <dearva@yandex.ru>
2023-05-06 14:24:29 +0900

    ...

---
+++ inout.h
@@ -0,0 +1,15 @@
+//
+// Created by darte on 06.05.2023.
+//
+
+#ifndef INC_1ST_INOUT_H
+#define INC_1ST_INOUT_H
+
+#include <vector>
+void print(const std::vector<int> &vec);
+void read(std::vector<int> &vec);
+
+
+#endif //INC_1ST_INOUT_H
+
+
